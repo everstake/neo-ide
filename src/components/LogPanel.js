414ab127ee8d1d8a91b7be223ddc7990147d00e7
Oleksandr Barannik <oleksandr.b@blaize.tech>
2019-10-08 15:58:15 +0300

    Logger timestamps added

--- src/components/LogPanel.js
+++ src/components/LogPanel.js
@@ -1,7 +1,14 @@
  
-import React from "react";
-// import '../stylesheets/d.css';
+import React, { Component } from 'react';
+
 import ReactTerminal from 'react-terminal-component';
+import {ReactTerminalStateless} from 'react-terminal-component';
+
+import {
+    EmulatorState, OutputFactory, CommandMapping,
+    EnvironmentVariables, FileSystem, History,
+    Outputs, defaultCommandMapping
+  } from 'javascript-terminal';
 
 const theme={
   textAlign: 'left',
@@ -14,20 +21,50 @@ const theme={
   spacing: '1%',
   fontFamily: 'monospace',
   width: '100%',
-  height: '50vh',
-  // height: '100%'
+  height: '30vh',
+//   height: '100%'
 }
+
+const defaultState = EmulatorState.createEmpty();
+const defaultOutputs = defaultState.getOutputs();
+
+function getTime() {
+    const timestamp = Date.now();
+
+    return new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit', dayPeriod: ''}).format(timestamp) + ' '
+}
+
+let newOutputs = Outputs.addRecord(
+    defaultOutputs, OutputFactory.makeTextOutput(
+        getTime() + `This is an example of using output to display a message to the user before any commands are run.`
+    )
+);
+
+newOutputs = Outputs.addRecord(
+    newOutputs, OutputFactory.makeTextOutput(
+        getTime() + 'Ha, ha lo lo'
+    )
+);
+
+const emulatorState = defaultState.setOutputs(newOutputs);
+
 class LogPanel extends React.Component
 {
 
 constructor(props){
 
-    super(props)
+    super(props);
+
+    // this.state = {
+    //     emulatorState: EmulatorState.createEmpty(),
+    //     inputStr: 'initial value'
+    // };
 }
 
 render (){
     return (           
-        <ReactTerminal theme={theme}/>
+        <ReactTerminal inputStr=''
+        emulatorState={emulatorState}/>
     );
     }
 
