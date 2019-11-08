6c65657985ebb202c2d36a4880e1a4cfc71c5a64
oleksandr.b <oleksandr.b@blaize.tech>
2019-11-08 12:07:20 +0200

    .gitignore modified

--- compilers/create_env_for_c#_compiler.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('git clone https://github.com/neo-project/neo-compiler.git')
-os.system('cd./neo-compiler/neon')
-os.system('dotnet')
-os.system('publish-o../../testlib')
-os.system('cd../..')
