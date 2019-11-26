6bc5d0b5dea2bcdb8899016bfb1b4039cd4c3760
David <david.k@blaize.tech>
2019-11-26 21:05:28 +0200

    Alter styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,13 @@ import MonacoEditor from "react-monaco-editor";
 import { connect } from "react-redux";
 import * as actions from "./actions/index";
 
-import * as Config from "Config";
-
 class App extends React.Component<any, any> {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            width: window.innerWidth,
             height: window.innerHeight,
-        };
-        this.onChange = this.onChange.bind(this);
-    }
-
-    updateDimensions = () => {
-        this.setState({
             width: window.innerWidth,
-        });
-    };
-
-    componentDidMount() {
-        window.addEventListener("resize", this.updateDimensions.bind(this));
-    }
-
-    editorDidMount(editor, monaco) {
-        console.log("editorDidMount", editor);
-        editor.focus();
+        };
     }
 
     onChange(newValue, e) {
@@ -38,33 +20,12 @@ class App extends React.Component<any, any> {
         );
     }
 
-    editorWillMount(monaco) {
-        console.log(monaco);
-        // monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-        //     validate: true,
-        //     schemas: [{
-        //         uri: "http://myserver/foo-schema.json",
-        //         fileMatch: ['*'],
-        //         schema: {
-        //             type: "object",
-        //             properties: {
-        //                 p1: {
-        //                     enum: [ "v1", "v2"]
-        //                 },
-        //                 p2: {
-        //                     $ref: "http://myserver/bar-schema.json"
-        //                 }
-        //             }
-        //         }
-        //     }]
-        // });
-    }
-
     render() {
         const options = {
             selectOnLineNumbers: true,
             readOnly: !this.props.currentFile,
-            wordBasedSuggestions: this.props.autocomplete
+            wordBasedSuggestions: this.props.autocomplete,
+            automaticLayout: true,
         };
         let editorContent = this.props.value;
         if (!this.props.currentFile) {
@@ -79,8 +40,6 @@ class App extends React.Component<any, any> {
                 value={editorContent}
                 options={options}
                 onChange={this.onChange}
-                editorDidMount={this.editorDidMount}
-                editorWillMount={this.editorWillMount}
             />
         );
     }
@@ -100,7 +59,7 @@ const mapStateToProps = (store) => {
         currentFile: store.currentFile,
         fileLang: fileLang,
         autosave: store.settings.autosave,
-        autocomplete: store.settings.autocomplete
+        autocomplete: store.settings.autocomplete,
     };
 };
 
