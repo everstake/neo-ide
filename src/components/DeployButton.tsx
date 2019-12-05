bf820400d4af98a530e35206234d02506a1e319c
Oleksandr Barannik <oleksandr.b@blaize.tech>
2019-12-05 14:06:26 +0200

    Removed the ability to deploy without parameters

--- src/components/DeployButton.tsx
+++ src/components/DeployButton.tsx
@@ -2,18 +2,27 @@ import Telegram from "@material-ui/icons/Telegram";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import * as actions from "../actions/index";
 import { connect } from "react-redux";
-import notify from "../utils/notificator";
-import neoDapi from "neo-dapi";
-import * as Config from "Config";
-import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles(theme => ({
     button: {
         margin: theme.spacing(1),
     },
 }));
+
+const mouseClickEvents = ["mousedown", "click", "mouseup"];
+function simulateMouseClick(element) {
+    mouseClickEvents.forEach(mouseEventType =>
+        element.dispatchEvent(
+            new MouseEvent(mouseEventType, {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+                buttons: 1,
+            }),
+        ),
+    );
+}
 
 function CustomButtonView(props) {
     const classes = useStyles(props);
@@ -35,37 +44,8 @@ class CustomButton extends React.Component<any, any> {
     }
 
     deploy() {
-        // console.log(this.props.file);
-        const args = {
-            network: this.props.neo.network + "",
-            name: "nefff", //this.props.deployfield.map(f => f.name)[0] + "",
-            version: "1.0.1", //this.props.deployfield.map(f => f.version)[0] + "",
-            author: "fuggy", //this.props.deployfield.map(f => f.author)[0] + "",
-            email: "fuggy", //this.props.deployfield.map(f => f.emai)[0] + "",
-            description: "fuggy", //this.props.deployfield.map(f => f.description)[0] + "",
-            needsStorage: false, //this.props.deployfield.map(f => f.needsStorage)[0],
-            dynamicInvoke: false, //this.props.deployfield.map(f => f.dynamicInvoke)[0],
-            isPayable: false, //this.props.deployfield.map(f => f.isPayable)[0],
-            parameterList: "0710",
-            returnType: "05",
-            code: this.props.file.binary + "",
-            networkFee: Config.deploy.defaultFee + "",
-        };
-        neoDapi.deploy(args).then((result) => {
-            const msg = `Transaction has been successfully deployed!\nTransaction ID:\n    ${result.txid} (viewing the transaction by reference will be available after adding it to the block)`;
-            this.props.addLog(msg, "Deploy");
-            this.props.enqueueSnackbar(notify("Transaction has been successfully deployed!", "success", "Deploy", this.props.closeSnackbar));
-            this.props.changeFileDeployed(this.props.file.key, result.txid);
-
-        }).catch(err => {
-            console.log(err);
-            this.props.addLog(err.description.message || err.description || "Transaction has been rejected!", "Deploy");
-            this.props.enqueueSnackbar(notify(err.description.message || err.description || "Transaction has been rejected!", "error", "Deploy", this.props.closeSnackbar));
-            // Error with bed specification https://github.com/NeoResearch/neocompiler-eco/issues/45
-            if (err.description.message && (err.description.message === "Error: One of the Policy filters failed.")) {
-                this.props.enqueueSnackbar(notify("Try to increase the amount of fee", "info", "Deploy", this.props.closeSnackbar));
-            }
-        });
+        const element = document.querySelector("i[class=\"fa fa-fw fa-play-circle\"]");
+        simulateMouseClick(element);
     }
 
     render() {
@@ -92,17 +72,7 @@ const mapStateToProps = (store) => {
     });
     return {
         file: file,
-        neo: store.neo,
-        contract: store.contract,
-        deployfield: store.deployfield.filter(f => f.contract === store.contract.map(f => f.contract)[0]),
     };
 };
 
-const mapDispatchToProps = dispatch => ({
-    changeFileDeployed: (name, i) => dispatch(actions.changeFileDeployed(name, i)),
-    enqueueSnackbar: (message, options) => dispatch(actions.enqueueSnackbar(message /*options*/)),
-    closeSnackbar: (key) => dispatch(actions.closeSnackbar(key)),
-    addLog: (a, b) => dispatch(actions.addLog(a, b)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(CustomButton);
+export default connect(mapStateToProps)(CustomButton);
